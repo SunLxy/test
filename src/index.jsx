ba1197efc8260403e487d2acfbc8b5a762b59d3c
SunLxy <1011771396@qq.com>
2022-05-08 12:48:04 +0800

    test:测试

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { unstable_HistoryRouter } from 'react-router-dom';
+import { unstable_HistoryRouter, } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import App from './App';
 import './styles/index.less';
 import ReactClient from 'react-dom/client';
+export const history = createBrowserHistory();
+
 const HistoryRouter = unstable_HistoryRouter
 
 ReactClient.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HistoryRouter basename={process.env.PUBLIC_URL}>
+    <HistoryRouter history={history} basename={process.env.PUBLIC_URL}>
       <App />
     </HistoryRouter>
   </React.StrictMode>,
